a5530d84397ee5b281f951c6b5b6d408e635d79c
Codegirlemem <eduokuemem@gmail.com>
2026-02-26 02:12:54 +0100

    fix deploy failure

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config({ quiet: process.env.NODE_ENV === "production" });
+
 import app from "./app.js";
 import connectDB from "./config/database.config.js";
 import appEnv from "./config/env.config.js";
 
-const PORT = appEnv.PORT;
+const port = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
     await connectDB();
 
-    app.listen(PORT, () => {
-      `Server running in ${appEnv.NODE_ENV} mode on port ${PORT}`;
+    app.listen(port, () => {
+      `Server running in ${appEnv.NODE_ENV} mode on port ${port}`;
     });
   } catch (error) {
     console.error("Failed to start server", error);
